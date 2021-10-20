ff143192bb44d54c9c4fb8a52cabf35acdae1935
Thiago Silva <dudaprofe97@gmail.com>
2021-10-20 08:51:34 -0300

    Inicio do projeto Clinica API

---
+++ src/main/java/br/com/thiagosilva/clinicaapi/ClincaapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.thiagosilva.clinicaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClincaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClincaapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/thiagosilva/clinicaapi/ClincaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thiagosilva.clinicaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClincaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
